2af3ff1f3740690138537f2fcb36e6689a40794d
elizatuinei <55069280+elizatuinei@users.noreply.github.com>
2024-03-10 18:37:49 -0600

    commitchanges

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import movieData from './MovieData.json';
 
+const mds = movieData.MovieDataBlah;
+
+function MovieList() {
+  const [listOfMovies, setListOfMovies] = useState(mds);
+  const addMovie = () => {
+    setListOfMovies([
+      ...mds,
+      {
+        Category: 'Action/Adventure',
+        Title: 'Batman Returns',
+        Year: 1992,
+        Director: 'Time Burton',
+        Rating: 'PG-13',
+      },
+    ]);
+  };
+  return (
+    <>
+      <h3>Movie Collection List</h3>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Year</th>
+            <th>Director</th>
+            <th>Rating</th>
+            <th>Category</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listOfMovies.map((m) => (
+            <tr>
+              <td>{m.Title}</td>
+              <td>{m.Year}</td>
+              <td>{m.Director}</td>
+              <td>{m.Rating}</td>
+              <td>{m.Category}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      <button onClick={addMovie}>Add Movie</button>
+    </>
+  );
+}
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieList />
     </div>
   );
 }
-
+// this becomes a tag that you use to be able to render
+// everything in the index
 export default App;
